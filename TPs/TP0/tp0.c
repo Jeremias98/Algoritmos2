0946c7c4867d8486b67963f0d6ca27d63c40a907
Jeremias <ramirezjeremias98@gmail.com>
2018-09-06 21:39:43 -0300

    Primeras correcciones hechas

--- TPs/TP0/tp0.c
+++ TPs/TP0/tp0.c
@@ -6,7 +6,6 @@
  * */
 
 #include "tp0.h"
-#include<stdio.h>
 
 /* ******************************************************************
  *                     FUNCIONES A COMPLETAR
@@ -16,8 +15,7 @@
  */
 void swap (int* x, int* y) {
 	// Creo una variable auxiliar y hago el intercambio de valores
-	int aux;
-	aux = *x;
+	int aux = *x;
 	*x = *y;
 	*y = aux;
 }
@@ -26,24 +24,21 @@ void swap (int* x, int* y) {
  * Si el vector es de largo 0, devuelve -1.
  */
 int maximo(int vector[], int n) {
-	/* En maximo guardo el valor a comparar, y 
-	 en max_index su posicion */
-	int maximo = 0;
+	
 	int max_index = 0;
-	// Compruebo que el vector tenga elementos, si no retorno -1
-	if (n > 0) {
-		// Guardo en maximo el primer elemento
-		maximo = vector[0];
-		for(int i = 0; i < n ; i++) {
-			// Recorro elemento a elemento, si es mayor lo guardo
-			if (vector[i] >= maximo) {
-				maximo = vector[i];
-				max_index = i;
-			}
-		}
-		return max_index;
+	
+	if (n <= 0) {
+		return -1;
 	}
-	return -1;
+	
+	for(int i = 0; i < n ; i++) {
+		if (vector[i] >= vector[max_index]) {
+			max_index = i;
+		}
+	}
+	
+	return max_index;
+	
 }
 
 /* La función comparar recibe dos vectores y sus respectivas longitudes
@@ -55,16 +50,11 @@ int maximo(int vector[], int n) {
  */
 int comparar(int vector1[], int n1, int vector2[], int n2) {
 	
-	/* Quiero obtener el menor n, para eso guardo alguno de los dos en n 
-	 * y verifico si son distintos */
 	int n = n1;
 	if (n1 != n2) {
-		// Con un operador ternario guardo el menor de los valores en n
 		n = (n1 < n2) ? n1 : n2;
 	}
-	// Si hubiesen sido iguales, cualquier n sirve
 
-	// Comparo elemento a elemento y retorno en caso de ser necesario
 	for (int i = 0; i < n ; i++) {
 		if (vector1[i] < vector2[i]) {
 			return -1;
@@ -74,14 +64,11 @@ int comparar(int vector1[], int n1, int vector2[], int n2) {
 		}
 	}
 	
-	/* Llegado a este punto, si no se retorno nada es porque eran iguales 
-	 * o no tenian elementos. Verifico cual caso es con un if */
 	if (n1 != n2) {
 		return (n1 < n2) ? -1 : 1;
 	}
-	else {
-		return 0;
-	}
+	
+	return 0;
 	
 }
 
@@ -89,15 +76,14 @@ int comparar(int vector1[], int n1, int vector2[], int n2) {
  * selección.
  */
 void seleccion(int vector[], int n) {
+	
+	/* Se reutiliza codigo implementando la funcion maximo en el algoritmo de seleccion */
 	for (int i = 0; i < n-1 ; i++) {
-		int minimo = i;
-		for (int j = i+1; j < n ; j++) {
-			if (vector[j] < vector[minimo]) {
-				minimo = j;
-			}
-		}
-		swap(&vector[i], &vector[minimo]);
+		int max = maximo(vector, n-i);
+		swap(&vector[n-1-i], &vector[max]);
 	}
+	
+	
 }
 
 
